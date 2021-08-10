d8e213c74e3827fa562274c5c01e96712f058a5a
Melih Çalışkan <melih.caliskan@tapu.com>
2021-08-10 21:24:51 +0300

    comment fetch

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,24 +1,32 @@
 export const loginRequest = (body) => {
-  const data = {
-    "name": "...",
-    "email": "...",
-    "phonenumber": "000000000000",
-    "country_code": "TR",
-    "text": "..."
-  }
+  console.log("Prepared data:", body);
 
-  console.log("Prepared data:", data);
+  // fetch('https://example.com/', {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json', },
+  //   body: JSON.stringify(data),
+  // }).then(response => response.json())
+  //   .then(data => {
+  //     console.log('Success:', data);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error:', error);
+  //   });
 
-  fetch('https://example.com/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', },
-    body: JSON.stringify(data),
-  }).then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+}
 
+export const contactRequest = (body) => {
+  console.log("Prepared data:", body);
+
+  // fetch('https://example.com/', {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json', },
+  //   body: JSON.stringify(body),
+  // }).then(response => response.json())
+  //   .then(data => {
+  //     console.log('Success:', data);
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error:', error);
+  //   });
 }
