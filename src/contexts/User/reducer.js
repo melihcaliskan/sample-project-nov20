ba1bde23a1060a97eff18c7cc82d06ec38c409d2
Melih Çalışkan <melih.caliskan@tapu.com>
2021-08-10 21:25:34 +0300

    create user context & reducer

---
+++ src/contexts/User/reducer.js
@@ -0,0 +1,29 @@
+export const reducer = (state, action) => {
+  console.log(state, action)
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        ...action.payload,
+        loggedIn: true
+      }
+    case "contact":
+      return {
+        ...state,
+        ...action.payload,
+      }
+    case "logout":
+      return {
+        initialState,
+      }
+    default:
+      return state
+  }
+}
+
+export const initialState = {
+  loggedIn: false,
+  id: "",
+  name: "",
+  email: ""
+}
