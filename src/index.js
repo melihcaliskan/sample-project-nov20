64396b3e9afa36d49ab4f5a8deb132b9c1f89e54
Melih Çalışkan <melih.caliskan@tapu.com>
2021-08-15 01:07:24 +0300

    move i18n & locales to separate files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+import './i18n';
 import './index.css';
 
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
 import { IoMenu } from "react-icons/io5";
 import { Container, Drawer, Button, Group, Grid, Col } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
@@ -15,48 +14,6 @@ import Contact from './pages/Contact';
 import LeftMenu from './components/LeftMenu';
 import RightMenu from './components/RightMenu';
 import Footer from './components/Footer';
-// TODO: Dosyaya taşınacak.
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: {
-          "home": "Home",
-          "contact": "Contact",
-          "hello": "Hello!",
-          "lorem": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in convallis lectus, et feugiat velit. Curabitur et est tincidunt, tincidunt erat et, dictum sem. Aliquam mollis orci id congue blandit.",
-          "login": "Login",
-          "name": "Name",
-          "email": "E-mail",
-          "email_incorrect": "Your e-mail is incorrect",
-          "password": "Şifre",
-          "strong_password": "Strong password should include letters in lower and uppercase, at least 1 number, at least 1 special symbol",
-          "send": "Send",
-          "current_page": "Current Page"
-        }
-      },
-      tr: {
-        translation: {
-          "home": "Anasayfa",
-          "contact": "İletişim",
-          "hello": "Merhaba!",
-          "lorem": "In pulvinar eros auctor orci rutrum, in egestas ante consequat. Vivamus non nisl eu odio viverra finibus et id enim. Proin sagittis turpis eu sodales porta.",
-          "login": "Giriş Yap",
-          "name": "İsim",
-          "email": "E-posta",
-          "email_incorrect": "Your e-mail is incorrect",
-          "password": "Şifre",
-          "strong_password": "Şifre küçük harf, büyük harf, en az 1 rakam, en az 1 özel sembol içermelidir.",
-          "send": "Gönder",
-          "current_page": "Şuanki Sayfa"
-        }
-      }
-    },
-    lng: "en",
-    fallbackLng: "en"
-  });
-
 
 const MobileLeftMenu = () => {
   return (
@@ -156,6 +113,8 @@ function App() {
 }
 
 ReactDOM.render(
-  <App />,
+  <Suspense fallback={<p>Loading...</p>}>
+    <App />
+  </Suspense>,
   document.getElementById("root")
 );
